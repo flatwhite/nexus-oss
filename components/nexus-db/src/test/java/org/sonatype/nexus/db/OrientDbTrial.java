7d27ca00fefbde386efb30861080f139709a6910
Jason Dillon <jason@planet57.com>
2014-06-19 16:57:06 -0700

    Add a test of using ODatabaseDocumentTx with "local:" usage

--- components/nexus-db/src/test/java/org/sonatype/nexus/db/OrientDbTrial.java
+++ components/nexus-db/src/test/java/org/sonatype/nexus/db/OrientDbTrial.java
@@ -1,19 +1,21 @@
 package org.sonatype.nexus.db;
 
+import java.io.File;
 import java.net.URL;
 
 import org.sonatype.sisu.litmus.testsupport.TestSupport;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.server.OServer;
 import com.orientechnologies.orient.server.OServerMain;
-import com.orientechnologies.orient.server.config.OServerConfiguration;
 import org.junit.Test;
 
 /**
  * Trials of using OrientDB.
  */
 public class OrientDbTrial
-  extends TestSupport
+    extends TestSupport
 {
   @Test
   public void embeddedServerTest() throws Exception {
@@ -23,5 +25,30 @@ public class OrientDbTrial
     server.startup(config.openStream());
     server.activate();
     server.shutdown();
+  }
+
+  @Test
+  public void documentTxTest() throws Exception {
+    File dir = util.createTempDir("testdb");
+    ODatabaseDocumentTx db = new ODatabaseDocumentTx("local:" + dir.getPath());
+    if (!db.exists()) {
+      db.create();
+      log("DB created");
+    }
+    else {
+      db.open("admin", "admin");
+      log("DB opened");
+    }
+
+    try {
+      ODocument doc = new ODocument("Person");
+      doc.field("name", "Luke");
+      doc.field("surname", "Skywalker");
+      doc.field("city", new ODocument("City").field("name", "Rome").field("country", "Italy"));
+      doc.save();
+    }
+    finally {
+      db.close();
+    }
   }
 }
